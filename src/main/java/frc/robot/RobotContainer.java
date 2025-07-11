ce187a9d0ac6341f361703cf2b24c2f41448e400
caTr1x <75480109+catr1xLiu@users.noreply.github.com>
2025-07-11 16:38:52 -0400

    upgraded project to latest version (wpilib vers. 25.3.2)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -152,8 +152,8 @@ public class RobotContainer {
         // Reset gyro / odometry
         final Runnable resetGyro = Constants.currentMode == Constants.Mode.SIM
                 ? () -> drive.setPose(
-                        driveSimulation
-                                .getSimulatedDriveTrainPose()) // reset odometry to actual robot pose during simulation
+                        driveSimulation.getSimulatedDriveTrainPose()) // reset odometry to actual robot pose during
+                // simulation
                 : () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), new Rotation2d())); // zero gyro
         controller.start().onTrue(Commands.runOnce(resetGyro, drive).ignoringDisable(true));
 
